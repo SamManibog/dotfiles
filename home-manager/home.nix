803c1b9097ac6390806ed1e6c0b5481ffbae9267
Sam Manibog <sfmanibog@outlook.com>
2025-05-13 20:47:41 -1000

    minor fix to indenting of files

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,78 +1,78 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
+inputs,
+lib,
+config,
+pkgs,
+...
 }: {
-  # You can import other home-manager modules here
-  imports = [
-  ];
-
-  nixpkgs = {
-    overlays = [
+    # You can import other home-manager modules here
+    imports = [
     ];
 
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
+    nixpkgs = {
+        overlays = [
+        ];
+
+        # Configure your nixpkgs instance
+        config = {
+            # Disable if you don't want unfree packages
+            allowUnfree = true;
+            # Workaround for https://github.com/nix-community/home-manager/issues/2942
+            allowUnfreePredicate = _: true;
+        };
     };
-  };
 
-  home = {
-    username = "sam";
-    homeDirectory = "/home/sam";
+    home = {
+        username = "sam";
+        homeDirectory = "/home/sam";
 
-	packages = [ pkgs.neofetch ];
+        packages = [ pkgs.neofetch ];
 
-	files = {
-		hyprland_config = {
-			target = ".config/hypr/hyprland.conf";
-			source = "../configs/hyprland.conf";
-			recursive = true;
-			force = true;
-		};
+        files = {
+            hyprland_config = {
+                target = ".config/hypr/hyprland.conf";
+                source = "../configs/hyprland.conf";
+                recursive = true;
+                force = true;
+            };
 
-		file.waybar_config = {
-			target = ".config/waybar/config";
-			source = "../configs/waybar.conf";
-			recursive = true;
-			force = true;
-		};
-	};
-  };
+            file.waybar_config = {
+                target = ".config/waybar/config";
+                source = "../configs/waybar.conf";
+                recursive = true;
+                force = true;
+            };
+        };
+    };
 
-  programs = {
-	  home-manager.enable = true;
+    programs = {
+        home-manager.enable = true;
 
-	  kitty = {
-		  enable = true;
-		  settings = {
-			  confirm_os_window_close = 0;
-			  enable_audio_bell = "no";
-			  scrollback_lines = 5000;
-		  };
-	  };
+        kitty = {
+            enable = true;
+            settings = {
+                confirm_os_window_close = 0;
+                enable_audio_bell = "no";
+                scrollback_lines = 5000;
+            };
+        };
 
-	  git = {
-		  enable = true;
-		  userName = "Sam Manibog";
-		  userEmail = "sfmanibog@outlook.com";
-	  };
-  };
+        git = {
+            enable = true;
+            userName = "Sam Manibog";
+            userEmail = "sfmanibog@outlook.com";
+        };
+    };
 
-  services = {
-  	ssh-agent.enable = true;
-  };
+    services = {
+        ssh-agent.enable = true;
+    };
 
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+    # Nicely reload system units when changing configs
+    systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.11";
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    home.stateVersion = "24.11";
 }
